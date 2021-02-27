904eb32ce11fc870fd9e74518d82a1a4f3f8cee7
SgtMilk <alix.routhier-lalonde@hotmail.com>
2021-02-27 01:45:05 -0500

    update to pipeline

--- setup/helper/database.js
+++ setup/helper/database.js
@@ -36,24 +36,36 @@ connection.connect((err) => {
   } else console.log("Database connected");
 });
 
-let sql = `CREATE USER '${sql_parameters.username}'@'localhost' IDENTIFIED BY '${sql_parameters.password}';`;
+let sql = `select user from mysql.user where user = '${sql_parameters.username}';`;
 connection.query(sql, (err, result) => {
   if (err) throw err;
+  if (result[0] != null) {
+    sql = `CREATE USER '${sql_parameters.username}'@'localhost' IDENTIFIED BY '${sql_parameters.password}';`;
+    connection.query(sql, (err, result) => {
+      if (err) throw err;
+    });
+
+    sql = `GRANT ALL PRIVILEGES ON * . * TO '${sql_parameters.username}'@'localhost';`;
+    connection.query(sql, (err, result) => {
+      if (err) throw err;
+    });
+
+    sql = `FLUSH PRIVILEGES;`;
+    connection.query(sql, (err, result) => {
+      if (err) throw err;
+    });
+  }
 });
 
-sql = `GRANT ALL PRIVILEGES ON * . * TO '${sql_parameters.username}'@'localhost';`;
+sql = `SELECT schema_name FROM information_schema.schemata where schema_name = '${sql_parameters.database}';`;
 connection.query(sql, (err, result) => {
   if (err) throw err;
-});
-
-sql = `FLUSH PRIVILEGES;`;
-connection.query(sql, (err, result) => {
-  if (err) throw err;
-});
-
-sql = `CREATE DATABASE ${sql_parameters.database};`;
-connection.query(sql, (err, result) => {
-  if (err) throw err;
+  if (result[0] != null) {
+    sql = `CREATE DATABASE ${sql_parameters.database};`;
+    connection.query(sql, (err, result) => {
+      if (err) throw err;
+    });
+  }
 });
 
 connection.end();
