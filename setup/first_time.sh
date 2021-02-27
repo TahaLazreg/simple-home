1785a858f2a6ad9b21f28c7f361032cd5c7f48b1
SgtMilk <alix.routhier-lalonde@hotmail.com>
2021-02-27 01:06:26 -0500

    update to pipeline

--- setup/first_time.sh
+++ setup/first_time.sh
@@ -1,16 +1,16 @@
-sudo apt-get update
-sudo apt-get upgrade
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo apt-get install mariadb-server
+#sudo apt-get update
+#sudo apt-get upgrade
+#curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+#sudo apt-get install -y nodejs
+#sudo apt-get install mariadb-server
 cd ..
-npm update
+#npm update
 cd client
-npm update
-npm run build
+#npm update
+#npm run build
 cd ..
-sudo mysql_secure_installation
-mysql -u root -Bse "CREATE USER 'temp'@'localhost' IDENTIFIED BY 'qwertyui';GRANT ALL PRIVILEGES ON * . * TO 'temp'@'localhost';ALTER USER 'temp'@'localhost' IDENTIFIED WITH mysql_native_password BY 'qwertyui';GRANT ALL PRIVILEGES ON * . * TO 'temp'@'localhost';GRANT GRANT OPTION ON * . * TO 'temp'@'localhost';FLUSH PRIVILEGES;"
+#sudo mysql_secure_installation
+mysql -u root -Bse "CREATE USER 'temp'@'localhost' IDENTIFIED BY 'qwertyui';GRANT ALL PRIVILEGES ON * . * TO 'temp'@'localhost';ALTER USER 'temp'@'localhost' IDENTIFIED WITH mysql_native_password BY 'qwertyui';GRANT PROXY ON 'root'@'localhost' TO 'temp'@'localhost';"
 cd backend/constants
 node ../../setup/helper/database.js
 #mysql -u root -Bse "DROP USER 'temp'@'localhost'";
