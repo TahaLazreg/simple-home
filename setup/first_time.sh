83902c293788b6b467721f40ef2d57d23bac9b4b
SgtMilk <alix.routhier-lalonde@hotmail.com>
2021-02-27 02:38:22 -0500

    pipeline v1.0

--- setup/first_time.sh
+++ setup/first_time.sh
@@ -1,15 +1,15 @@
-#sudo apt-get update
-#sudo apt-get upgrade
-#curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-#sudo apt-get install -y nodejs
-#sudo apt-get install mariadb-server
+sudo apt-get update
+sudo apt-get upgrade
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt-get install mariadb-server
 cd ..
-#npm update
+npm update
 cd client
-#npm update
-#npm run build
+npm update
+npm run build
 cd ..
-#sudo mysql_secure_installation
+sudo mysql_secure_installation
 mysql -u root -Bse "CREATE USER 'temp'@'localhost' IDENTIFIED BY 'qwertyui';"
 mysql -u root -Bse "GRANT ALL PRIVILEGES ON * . * TO 'temp'@'localhost';"
 mysql -u root -Bse "FLUSH PRIVILEGES;"
