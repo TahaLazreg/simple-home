2d773dd0caa427155a0fe7348ca89e0b3504bef8
SgtMilk <alix.routhier-lalonde@hotmail.com>
2021-02-27 01:20:56 -0500

    update to pipeline

--- setup/first_time.sh
+++ setup/first_time.sh
@@ -10,7 +10,11 @@ cd client
 #npm run build
 cd ..
 #sudo mysql_secure_installation
-mysql -u root -Bse "CREATE USER 'temp'@'localhost' IDENTIFIED BY 'qwertyui';GRANT ALL PRIVILEGES ON * . * TO 'temp'@'localhost';ALTER USER 'temp'@'localhost' IDENTIFIED WITH mysql_native_password BY 'qwertyui';GRANT ALL PRIVILEGES ON * . * TO 'temp'@'localhost';FLUSH PRIVILEGES;GRANT GRANT OPTION ON * . * TO 'temp'@'localhost';"
+mysql -u root -Bse "CREATE USER 'temp'@'localhost' IDENTIFIED BY 'qwertyui';"
+mysql -u root -Bse "ALTER USER 'temp'@'localhost' IDENTIFIED WITH mysql_native_password BY 'qwertyui';"
+mysql -u root -Bse "GRANT ALL PRIVILEGES ON * . * TO 'temp'@'localhost';"
+mysql -u root -Bse "FLUSH PRIVILEGES;"
+mysql -u root -Bse "GRANT GRANT OPTION ON * . * TO 'temp'@'localhost';"
 cd backend/constants
 node ../../setup/helper/database.js
 #mysql -u root -Bse "DROP USER 'temp'@'localhost'";
